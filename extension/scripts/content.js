c0f1cfa7513fa1d756620c9fc04fa8564821b880
Mikolaj Markiewicz <kajo100@gmail.com>
2020-04-30 22:11:16 +0200

    Span class update

--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -2,7 +2,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   var data = request.data || {};
 
   var names = [];
-  var links = document.querySelectorAll('.dati1w0a.ihqw7lf3.hv4rvrfc.discj3wi div.buofh1pr.hv4rvrfc a > span.oi732d6d.ik7dh3pa.d2edcug0.qv66sw1b.c1et5uql.a8c37x1j.s89635nw.ew0dbk1b.a5q79mjw.g1cxx5fr.hnhda86s.oo9gr5id');
+  var links = document.querySelectorAll('.dati1w0a.ihqw7lf3.hv4rvrfc.discj3wi div.buofh1pr.hv4rvrfc a > span.oi732d6d.ik7dh3pa.d2edcug0.qv66sw1b.c1et5uql.a8c37x1j.s89635nw.ew0dbk1b.a5q79mjw.g1cxx5fr.lrazzd5p.oo9gr5id');
   links.forEach(function (link) {
     names.push(link.textContent);
   });
